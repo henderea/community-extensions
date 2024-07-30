db56c118903e8afb1052d2ce0d449870a88d67c8
Eric Henderson <henderea@gmail.com>
2024-07-30 12:10:21 -0400

    fix a missed update and also revert the paperback packages to something that works

--- src/MangaDex/MangaDexParser.ts
+++ src/MangaDex/MangaDexParser.ts
@@ -1,6 +1,7 @@
 import { PartialSourceManga } from '@paperback/types'
 import { MDImageQuality } from './MangaDexHelper'
 import { MangaItem } from './MangaDexInterfaces'
+import { decode as decodeHTMLEntity } from 'html-entities'
 
 
 export const parseMangaList = async (object: MangaItem[], source: any, thumbnailSelector: any): Promise<PartialSourceManga[]> => {
@@ -9,7 +10,7 @@ export const parseMangaList = async (object: MangaItem[], source: any, thumbnail
     for (const manga of object) {
         const mangaId = manga.id
         const mangaDetails = manga.attributes
-        const title = source.decodeHTMLEntity(mangaDetails.title.en ?? mangaDetails.altTitles.map(x => Object.values(x).find((v) => v !== undefined)).find((t) => t !== undefined))
+        const title = decodeHTMLEntity(mangaDetails.title.en ?? mangaDetails.altTitles.map(x => Object.values(x).find((v) => v !== undefined)).find((t) => t !== undefined))
         const coverFileName = manga.relationships.filter((x) => x.type == 'cover_art').map((x) => x.attributes?.fileName)[0]
         const image = coverFileName ? `${source.COVER_BASE_URL}/${mangaId}/${coverFileName}${MDImageQuality.getEnding(await thumbnailSelector(source.stateManager))}` : 'https://mangadex.org/_nuxt/img/cover-placeholder.d12c3c5.jpg'
         const subtitle = `${mangaDetails.lastVolume ? `Vol. ${mangaDetails.lastVolume}` : ''} ${mangaDetails.lastChapter ? `Ch. ${mangaDetails.lastChapter}` : ''}`
